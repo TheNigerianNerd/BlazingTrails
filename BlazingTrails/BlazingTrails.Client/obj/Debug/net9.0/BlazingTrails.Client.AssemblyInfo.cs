94031ce6248a04c47fe64b30388b1216ed7b1e77
TheNigerianNerd <142974826+TheNigerianNerd@users.noreply.github.com>
2025-10-07 22:01:54 -0400

    set up solution file alongside relevant tooling

--- BlazingTrails/BlazingTrails.Client/obj/Debug/net9.0/BlazingTrails.Client.AssemblyInfo.cs
+++ BlazingTrails/BlazingTrails.Client/obj/Debug/net9.0/BlazingTrails.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingTrails.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ac3bce886d139c18da5f01e158861d28d895b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingTrails.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingTrails.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
