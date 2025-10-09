0be64f53607ab34fd27a56239687eb1b58b89396
TheNigerianNerd <142974826+TheNigerianNerd@users.noreply.github.com>
2025-10-09 09:42:24 -0400

    Create API to serve Blazor application as well as a shared csharp class for shared functionality

---
+++ BlazingTrails/BlazingTrails.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
